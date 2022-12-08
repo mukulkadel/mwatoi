fa3acdc85ef48faaf9d77646f8feb41f3b5ed85c
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-08 14:24:14 +0000

    Adding tarfile member sanitization to extractall()

--- main.py
+++ main.py
@@ -70,7 +70,26 @@ if not use_android_backup:
                 out.write(b)
 
     with tarfile.open('tmp\whatsapp.tar') as tp:
-        tp.extractall(path='tmp')
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tp, path="tmp")
 
     print('Creating out directory.')
     if not os.path.exists('out'):
